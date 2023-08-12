46ee0b3ce647447f79e6f81c351f2b813aaf52f2
Wade <wz.lim.00@gmail.com>
2023-08-12 18:27:52 +0800

    alternative voting if EAS not available

--- hardhat/contracts/TasteProtocol.sol
+++ hardhat/contracts/TasteProtocol.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {SchemaResolver} from "@ethereum-attestation-service/eas-contracts/contracts/resolver/SchemaResolver.sol";
 import {IEAS, Attestation} from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
 
-contract TasteProtocol is SchemaResolver {
+contract TasteProtocol is SchemaResolver, Ownable {
     using SafeERC20 for IERC20;
     struct RecipeData {
         string _recipeName;
@@ -32,9 +33,15 @@ contract TasteProtocol is SchemaResolver {
 
     uint256 public idCounter;
     address public tokenAddr;
+    bool public attestVote;
 
-    constructor(IEAS eas, address _tokenAddr) SchemaResolver(eas) {
+    constructor(
+        IEAS eas,
+        address _tokenAddr,
+        bool _attestVote
+    ) SchemaResolver(eas) {
         tokenAddr = _tokenAddr;
+        attestVote = _attestVote;
     }
 
     function requestRecipe(
@@ -67,9 +74,12 @@ contract TasteProtocol is SchemaResolver {
         newRecipe.ipfsFolderURL = _IPFSFolderURL;
 
         r.recipeSubmissions.push(newRecipe);
-        
+
         for (uint256 i = 0; i < authorRecipeIndex[msg.sender].length; i++) {
-            require(_id != authorRecipeIndex[msg.sender][i].requestID, "only one submission per request");
+            require(
+                _id != authorRecipeIndex[msg.sender][i].requestID,
+                "only one submission per request"
+            );
         }
         RecipeData memory newData;
         newData._recipeName = _recipeName;
@@ -79,7 +89,7 @@ contract TasteProtocol is SchemaResolver {
     }
 
     // EAS, hook attestation schema into this
-    function voteRecipe(
+    function _voteRecipe(
         uint256 _id,
         string memory _recipeName,
         address _recipeAuthor
@@ -124,15 +134,35 @@ contract TasteProtocol is SchemaResolver {
         IERC20(tokenAddr).safeTransfer(r.winner.author, r.reward);
     }
 
+    function voteRecipe(
+        uint256 _id,
+        string memory _recipeName,
+        address _recipeAuthor
+    ) external {
+        require(!attestVote, "Use Attestation to vote");
+        require(
+            voteMap[_id][msg.sender] == address(0),
+            "attester already voted on this request"
+        );
+        voteMap[_id][msg.sender] = _recipeAuthor;
+        _voteRecipe(_id, _recipeName, _recipeAuthor);
+    }
+
     function onAttest(
         Attestation calldata attestation,
         uint256 /*value*/
     ) internal override returns (bool) {
-        
-        (uint256 id, address author, string memory recipeName) = abi.decode(attestation.data, (uint256, address, string));
-        require(voteMap[id][attestation.attester] == address(0), "attester already voted on this request");
+        require(attestVote, "AttestVote not activated");
+        (uint256 id, address author, string memory recipeName) = abi.decode(
+            attestation.data,
+            (uint256, address, string)
+        );
+        require(
+            voteMap[id][attestation.attester] == address(0),
+            "attester already voted on this request"
+        );
         voteMap[id][attestation.attester] = author;
-        voteRecipe(id, recipeName, author);
+        _voteRecipe(id, recipeName, author);
         return true;
     }
 
@@ -141,5 +171,9 @@ contract TasteProtocol is SchemaResolver {
         uint256 /*value*/
     ) internal pure override returns (bool) {
         return true;
+    }
+
+    function setAttestVote(bool _attestVote) external onlyOwner {
+        attestVote = _attestVote;
     }
 }

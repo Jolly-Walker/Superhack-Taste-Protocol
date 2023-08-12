befebd6b0b13521ee33bce0d856aab56258d076f
Wade <wz.lim.00@gmail.com>
2023-08-12 23:16:21 +0800

    added deploy script

---
+++ scripts/deployTasteProtocol.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+const { addr } = require("../addresses.js");
+
+
+async function main() {
+  
+    const token = await hre.ethers.deployContract("TAS", []);
+    await token.waitForDeployment();
+    console.log("Token deployed to :", token.target);
+
+    const tasteProtocol = await hre.ethers.deployContract("TasteProtocol", [addr.OpGoerliEAS, token.target, true]);
+    console.log("TasteProtocol deployed to :", tasteProtocol.target);
+
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+  

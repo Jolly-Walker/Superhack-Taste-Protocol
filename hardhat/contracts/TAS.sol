d30300c34b740eb198cfe7eb0bf4a24937d30f31
Wade <wz.lim.00@gmail.com>
2023-08-12 20:56:00 +0800

    installed web3 storage, added addresses for deploy

--- hardhat/contracts/TAS.sol
+++ hardhat/contracts/TAS.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TAS is ERC20 {
-    constructor() ERC20("Name", "Symbol") {
+    constructor() ERC20("Token", "TAS") {
         _mint(msg.sender, 20000 ether);
+    }
+
+    function getToken(uint256 _amount) public {
+        _mint(msg.sender, _amount);
     }
 }

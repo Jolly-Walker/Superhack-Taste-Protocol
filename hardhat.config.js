99d7f6630f545715a99c72b8e6c374e8ad3e0da7
Wade <wz.lim.00@gmail.com>
2023-08-13 00:12:55 +0800

    added networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
-const { privateKey} = require('./secrets.json');
+const { privateKey } = require('./secrets.json');
 
 module.exports = {
   networks: {
     localhost: {
       chainId: 31337,
       url: "http://127.0.0.1:8545",
+    },
+    opGoerli: {
+      chainId: 420,
+      url : "https://goerli.optimism.io",
+      accounts : [privateKey]
+    },
+    baseGoerli: {
+      chainId: 84531,
+      url : "https://goerli.base.org",
+      accounts : [privateKey]
+    },
+    zoraGoerli: {
+      chainId: 999,
+      url : "https://goerli.optimism.io",
+      accounts : [privateKey]
+    },
+    modeSepolia: {
+      chainId: 919,
+      url : "https://sepolia.mode.network/",
+      accounts : [privateKey]
     },
   },
   solidity: {
